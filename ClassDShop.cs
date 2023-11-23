a0e96d2130ab33a3e113f6332bf26df3002cd40d
Anas-NAk <123668136+Anas-NAk@users.noreply.github.com>
2023-11-23 13:49:13 +0300

    Add files via upload

---
+++ ClassDShop.cs
@@ -0,0 +1,126 @@
+﻿using System;
+
+class My_main()
+{
+    static void Main()
+    {
+
+    }
+}
+
+
+namespace Model1
+{
+    class Customer
+    {
+        private string name;
+        private string address;
+    }
+
+    class Order
+    {
+        private DateTime date;
+        private string status;
+        private Customer customer; // Association with Customer
+
+        private List<OrderDetail> orderDetails; // Aggregation with OrderDetail
+
+        public void calcSubTotal()
+        {
+            // Implementation
+        }
+
+        public void calcTax()
+        {
+            // Implementation
+        }
+
+        public void calcTotal()
+        {
+            // Implementation
+        }
+
+        public void calcTotalWeight()
+        {
+            // Implementation
+        }
+    }
+
+    class OrderDetail
+    {
+        private int quantity;
+        private string taxStatus;
+        private Item item; // Association with Item
+
+        public void calcSubTotal()
+        {
+            // Implementation
+        }
+
+        public void calcTax()
+        {
+            // Implementation
+        }
+
+        public void calcWeight()
+        {
+            // Implementation
+        }
+    }
+
+    class Item
+    {
+        private int shippingWeight;
+        private string description;
+
+        public void calcSubTotal()
+        {
+            // Implementation
+        }
+
+        public void calcTax()
+        {
+            // Implementation
+        }
+
+        public void calcWeight()
+        {
+            // Implementation
+        }
+    }
+
+    class Payment
+    {
+        private float amount;
+        private Order order; // Association with Order
+    }
+
+    class Credit : Payment
+    {
+        private string name;
+        private string type;
+        private object expDate;
+
+        public void authorized()
+        {
+            // Implementation
+        }
+    }
+
+    class Check : Payment
+    {
+        private string name;
+        private string bankID;
+
+        public void authorized()
+        {
+            // Implementation
+        }
+    }
+
+    class Cash : Payment
+    {
+        private float cashTendered;
+    }
+}
+
